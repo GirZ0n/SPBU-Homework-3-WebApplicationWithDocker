bbfffdf8b0f869beeba35271f421bab9680d8316
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-14 19:21:55 +0300

    Added serialization and ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
     id("io.gitlab.arturbosch.detekt") version "1.6.0"
 }
 
@@ -10,8 +11,18 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion = "1.4.0"
+
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("io.ktor:ktor-client-serialization-jvm:$ktorVersion")
+    //implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-freemarker:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation(kotlin("stdlib-jdk8"))
 }
 
