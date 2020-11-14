ba6a0d104cd2ef5fcad214e1193abdc5e13f97e0
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-11-14 19:08:07 +0300

    Added circleci and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("io.gitlab.arturbosch.detekt") version "1.6.0"
 }
 
 group = "org.example"
@@ -10,7 +11,13 @@ repositories {
 }
 
 dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
     implementation(kotlin("stdlib-jdk8"))
+}
+
+detekt {
+    failFast = true // fail build on any finding
+    buildUponDefaultConfig = true // preconfigure defaults
 }
 
 tasks {
